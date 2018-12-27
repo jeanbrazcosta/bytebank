f11e1524b32b523e9f46e9ab66e3cf3983844e97
jbraz <jbraz@Ciantd.com>
2018-12-27 16:42:19 -0200

    segundo método

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -4,18 +4,30 @@ public class Conta {
 	double saldo;
 	int agencia;
 	int numero;
-	String titular;
+Cliente titular;
+
 	public void deposita(double valor) {
-		this.saldo = this.saldo + valor;
+		this.saldo += valor;
 	}
 
-	public boolean saca (double valor) {
-		
-		if(this.saldo >= this.valor) {
-			this.saldo=this.saldo - valor;
-			returne true;
+	public boolean saca(double valor) {
+
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			System.out.println("Consegui sacar");
+			return true;
 		} else {
-			returne false;
+			return false;
+		}
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
--- src/bytebank/CriaConta.java
+++ src/bytebank/CriaConta.java
@@ -3,11 +3,21 @@ package bytebank;
 public class CriaConta {
 
 	public static void main(String[] args) {
+		Cliente paulo = new Cliente();
+		paulo.nome = "paulo Silveira";
+		paulo.cpf = "2222223204";
+		paulo.profissao = "programador";
 Conta primeiraConta = new Conta();
 primeiraConta.saldo = 200;
+primeiraConta.
 System.out.println("meu saldo atual é:" + primeiraConta.saldo);
 primeiraConta.saca(50);
 System.out.println("O saldo após sacar é de:" + primeiraConta.saldo);
+Conta contaDaMarcela = new Conta();
+contaDaMarcela.deposita(1000);
+contaDaMarcela.transfere(50, primeiraConta);
+System.out.println("Agora a conta da marcela tem" + contaDaMarcela.saldo);
+System.out.println("E a primeira conta tem" + primeiraConta.saldo);
 	}
 
 }
